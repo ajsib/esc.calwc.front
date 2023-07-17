4948ba3042709b2b86e646e916d69e9474a1dc1f
ajsib <80181039+ajsib@users.noreply.github.com>
2023-07-17 19:28:56 +0000

    fr-en swap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
-import { Button, Box, Container } from "@mui/material";
+import { Button, Box, Container, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { NextPage } from 'next';
+import { useEffect, useState } from "react";
 
 const LandingPage: NextPage = () => {
   const router = useRouter();
+  const [isEnglish, setIsEnglish] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsEnglish((prevIsEnglish) => !prevIsEnglish);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const handleContinue = () => {
-    router.push("/en/");
-  };
-
-  const handleContinuer = () => {
-    router.push("/fr/");
+    router.push(isEnglish ? "/en/" : "/fr/");
   };
 
   return (
@@ -23,19 +29,27 @@ const LandingPage: NextPage = () => {
         alignItems: 'center'
       }}
     >
+      <Typography variant="h5" sx={{ textAlign: 'center' }}>
+        {isEnglish
+          ? "Welcome to the Canadian Army Land Warfare Centre's Online Service Portal"
+          : "Bienvenue sur le Portail de Services en ligne du Centre de Guerre Terrestre de l'armée Canadienne"}
+      </Typography>
+
       <Box 
         sx={{ 
           width: '100%', 
           display: 'flex', 
           justifyContent: 'space-around', 
-          alignItems: 'center' 
+          alignItems: 'center',
+          marginTop: '2rem'
         }}
       >
-        <Button variant="contained" color="success" onClick={handleContinue}>
-          Continue
-        </Button>
-        <Button variant="contained" color="primary" onClick={handleContinuer}>
-          Continuer
+        <Button 
+          variant="contained" 
+          color={isEnglish ? "success" : "primary"} 
+          onClick={handleContinue}
+        >
+          {isEnglish ? "Continue" : "Continuer"}
         </Button>
       </Box>
     </Container>
