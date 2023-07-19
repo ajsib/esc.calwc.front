a789b88fa522f67b5ca69e46917ec40da174ec66
Aidan <ajsibley6@gmail.com>
2023-07-18 21:05:18 -0400

    changes to dashboard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Box, Drawer, IconButton, List, ListItem, ListItemText } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useState } from 'react';
+import { Typography } from '@mui/material';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -18,34 +19,44 @@ const Navbar = () => {
     setOpen(open);
   };
 
-  const list = () => (
-    <Box
-      sx={{ width: 250 }}
-      role="presentation"
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
-    >
-      <List>
-        {/* Add your page links here as ListItems */}
-        <ListItem button>
-          <ListItemText primary="Page 1" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Page 2" />
-        </ListItem>
-      </List>
-    </Box>
-  );
+  const list = () => {
+    const handleMouseLeave = () => {
+      setOpen(false);
+    };
+  
+    return (
+      <Box
+        sx={{ width: 250 }}
+        role="presentation"
+        onMouseLeave={handleMouseLeave}
+      >
+        <List>
+          <ListItem disablePadding>
+            <ListItemText
+              primaryTypographyProps={{ variant: 'h6', sx: { fontWeight: 'bold', textAlign: 'center' } }}
+              primary="Dashboard"
+            />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Page 1" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Page 2" />
+          </ListItem>
+        </List>
+      </Box>
+    );
+  };
+  
 
   return (
-    <Box
-      onMouseEnter={toggleDrawer(true)}
-      onMouseLeave={toggleDrawer(false)}
-      onClick={toggleDrawer(!open)}
-      sx={{ width: open ? 250 : 60, position: 'fixed', top: 0, left: 0, height: '100vh', overflowX: 'hidden', zIndex: 999 }}
-    >
-      <IconButton>
-        <MenuIcon />
+    <Box sx={{ position: 'fixed', top: '50%', left: 0, transform: 'translateY(-50%)', zIndex: 999 }}>
+      <IconButton
+        onMouseEnter={toggleDrawer(true)}
+        onClick={toggleDrawer(!open)}
+        sx={{ bgcolor: 'background.default', borderRadius: '50%', padding: '10px' }} // Changed the styles for kebab menu
+      >
+        <MoreVertIcon />
       </IconButton>
       <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
         {list()}
